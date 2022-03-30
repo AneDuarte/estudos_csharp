6d055bf1d13146241a016d42125d5d8b95e9a499
AneDuarte <anart.lodes73@gmail.com>
2022-03-30 13:37:56 -0300

    iniciando estudos de poo

---
+++ Pessoas/Pessoa.cs
@@ -0,0 +1,8 @@
+namespace Pessoas
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ Pessoas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;
+            p1 = new Pessoa();
+            p2 = new Pessoa();
+
+            Console.WriteLine("Digite o nome e idade da primeira pessoa.");
+            p1.nome = Console.ReadLine();
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome e idade da segunda pessoa.");
+            p2.nome = Console.ReadLine();
+            p2.idade = int.Parse(Console.ReadLine());
+
+            if (p1.idade > p2.idade)
+            {
+                Console.WriteLine("A pessoa mais velha é " + p1.nome);
+            }
+            else if (p2.idade > p1.idade)
+            {
+                Console.WriteLine("A pessoa mais velha é " + p2.nome);
+            }
+            else
+            {
+                Console.WriteLine("Ambos tem a mesma idade.");
+            }
+
+        }
+    }
+}
