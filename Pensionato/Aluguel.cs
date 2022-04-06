4677dca6876538ad71860d931794e7a4d45ab310
AneDuarte <anart.lodes73@gmail.com>
2022-04-06 12:07:07 -0300

    Estudos sobre listas

--- Pensionato/Aluguel.cs
+++ Pensionato/Aluguel.cs
@@ -5,5 +5,10 @@ namespace Pensionato
         public string Nome { get; set; }
         public string Email { get; set; }
         public int Quarto { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Email: {Email}";
+        }
     }
 }
