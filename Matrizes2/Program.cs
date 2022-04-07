2d0afb3684ca4c94f17e9d9f0153af05ed27b2b5
AneDuarte <anart.lodes73@gmail.com>
2022-04-06 22:48:55 -0300

    Continuação do estudo de matrizes

---
+++ Matrizes2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Matrizes2{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Digite o número de linhas: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de colunas: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[m, n];
+
+            for(int i = 0; i < m; i++){
+
+                string[] valores = Console.ReadLine().Split(" ");
+
+                for(int j = 0; j < n; j++){
+                    matriz[i, j] = int.Parse(valores[j]);
+                }
+            }
+            Console.WriteLine();
+
+            Console.Write("Digite um valor da matriz: ");
+            int resp = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < m; i++){
+
+                for(int j = 0; j < n; j++){
+                    if(matriz[i,j] == resp){
+                        Console.WriteLine($"A posição do número é {i},{j}");
+                        if(j > 0){
+                            Console.WriteLine($"Esquerda {matriz[i, j-1]}");
+                        }
+                        if(j < n-1){
+                            Console.WriteLine($"Direita {matriz[i, j+1]}");
+                        }
+                        if(i > 0){
+                            Console.WriteLine($"Cima {matriz[i-1, j]}");
+                        }
+                        if(i < m-1){
+                            Console.WriteLine($"Baixo {matriz[i+1, j]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
